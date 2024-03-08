acc53b74ecc3849bd938c24d9bed68d7f2f3cfd2
Emilbou <emilien.boucard@edu.univ-fcomte.fr>
2024-03-08 09:06:17 +0100

    correction swiper js

--- src/components/SwiperHome.vue
+++ src/components/SwiperHome.vue
@@ -1,57 +1,55 @@
 <script setup lang="ts">
 import { register } from 'swiper/element/bundle';
-import { supabase } from '../supabase'
-import { RouterLink } from 'vue-router';
+import { supabase } from '../supabase';
+import { useRouter } from 'vue-router';
 import CardCarrousel from '../components/CardCarrousel.vue';
 
 register();
-const { data: Projet, error } = await supabase
-    .from('Projet')
-    .select('*')
-console.log(Projet)
+const router = useRouter();
+const { data: Projet, error } = await supabase.from('Projet').select('*');
+console.log(Projet);
 
+const handleSlideClick = (projet) => {
+  router.push({ name: '/projet/[id]', params: { id: projet.id } });
+};
 </script>
 
 <template>
-     <swiper-container 
-    
-            :slides-per-view="3"
-            :space-between="20"
-            :centered-slides="false"
-            :grab-cursor="true"
-            
-             :autoplay="{
-                 delay: 2500,
-                 disableOnInteraction: false,
-             }"
-            :loop="true"
-            :breakpoints="{
-                0: {
-                    slidesPerView: 1,
-                },
-                640: {
-                    slidesPerView: 2,
-                },
-                992: {
-                    slidesPerView: 3,
-                },
-                1200: {
-                    slidesPerView: 4,
-                }
-            }"
-            @swiperprogress="onProgress"
-            @swiperslidechange="onSlideChange" class="w-full ">
-            <RouterLink
-                    v-for="projet in Projet"
-                    :to="{
-                        name: '/projet/[id]',
-                        params: { id:projet.id },
-                    }">
-                
-                  <swiper-slide class="rounded-md overflow-hidden" >
-                    <CardCarrousel v-bind="projet"/>
-                </swiper-slide>
-            </RouterLink>
-
-            </swiper-container>
+  <swiper-container
+    :slides-per-view="3"
+    :space-between="20"
+    :centered-slides="false"
+    :grab-cursor="true"
+    :autoplay="{
+      delay: 2500,
+      disableOnInteraction: false,
+    }"
+    :loop="true"
+    :breakpoints="{
+      0: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    }"
+    @swiperprogress="onProgress"
+    @swiperslidechange="onSlideChange"
+    class="w-full"
+  >
+    <swiper-slide
+      v-for="projet in Projet"
+      :key="projet.id"
+      class="rounded-md overflow-hidden cursor-pointer"
+      @click="handleSlideClick(projet)"
+    >
+      <CardCarrousel v-bind="projet" />
+    </swiper-slide>
+  </swiper-container>
 </template>
