e6fa14ceee64e168c7f70f448fd567b94ef3aa8c
emilbou <emilien.boucard@edu.univ-fcomte.fr>
2024-03-09 21:48:58 +0100

    paramétrage technos, vuejs et wordpress

--- src/components/DynamicData.vue
+++ src/components/DynamicData.vue
@@ -7,22 +7,49 @@ import type { SchemaProjet } from '../types'
 const props = defineProps<SchemaProjet>()
 
 const showElement = ref(false)
+const WordpressShow = ref(false)
+const VuejsShow = ref(false)
 
+// Est-ce un projet scolaire ?
 supabase
   .from('Projet')
   .select('scolaireprojet')
   .then(({ data }) => {
     if (data) {
-      showElement.value = false // Réinitialiser showElement à false
+      showElement.value = false 
       data.forEach(row => {
-        // Mettre à jour showElement avec la valeur de scolaireprojet pour chaque entrée
         showElement.value = row.scolaireprojet ?? false
-        // Faites quelque chose avec showElement.value ici
         console.log(`ID ${row.id} : showElement = ${showElement.value}`)
       })
     }
   })
   .catch(console.error)
+
+  // Quelles technos ?
+  supabase
+  .from('Projet')
+  .select('Wordpress')
+  .then(({ data }) => {
+    if (data) {
+      WordpressShow.value = false 
+      data.forEach(row => {
+        WordpressShow.value = row.Wordpress?? false
+        console.log(`ID ${row.id} : WordpressShow = ${WordpressShow.value}`)
+      })
+    }
+  })
+  supabase
+  .from('Projet')
+  .select('Vuejs')
+  .then(({ data }) => {
+    if (data) {
+      VuejsShow.value = false 
+      data.forEach(row => {
+        VuejsShow.value = row.Vuejs?? false
+        console.log(`ID ${row.id} : VuejsShow = ${VuejsShow.value}`)
+      })
+    }
+  })
 </script>
 
 <template>
@@ -30,7 +57,7 @@ supabase
     <div class="flex justify-between">
       <h1>{{ props.nomprojet || 'Nom du projet non défini' }}</h1>
       <p v-if="showElement === null">Chargement...</p>
-      <p v-else-if="showElement === true" id="jeveuxquecamarcheici">Ceci est un projet réalisé dans le cadre de mes études</p>
+      <p v-else-if="showElement === true">Ceci est un projet réalisé dans le cadre de mes études</p>
       <p v-else>Projet perso</p>
     </div>
 
@@ -39,6 +66,17 @@ supabase
 
       <div class="flex flex-col">
         <p>{{ props.descriptionprojet }}</p>
+        
+        <div>  
+          <p v-if="WordpressShow === null">Chargement...</p>
+                <p v-else-if="WordpressShow === true">avec wordpress</p>
+                <p v-else>pas de wordpress</p>
+        </div>
+        <div>  
+          <p v-if="VuejsShow === null">Chargement...</p>
+                <p v-else-if="VuejsShow === true">avec Vuejs</p>
+                <p v-else>pas de Vuejs</p>
+        </div>
       </div>
     </div>
   </div>
