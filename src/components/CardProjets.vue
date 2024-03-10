5ae4059b7895f1d09a3a3b901e3f1f738045a7b8
emilbou <emilien.boucard@edu.univ-fcomte.fr>
2024-03-10 17:05:50 +0100

    mise en forme page projets avec styles différents 1/2

--- src/components/CardProjets.vue
+++ src/components/CardProjets.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
 import type { SchemaProjet } from '../types'
-defineProps<SchemaProjet>()
+
+const props = defineProps<{
+  isAlternate: boolean;
+} & SchemaProjet>();
 </script>
 
 <template>
-
-    <div class="flex w-full h-fit mr-4 flex-row ">
-        <img class="object-cover aspect-[16/9] w-1/2 rounded-xl overflow-hidden h-auto" :src="imageprojet" alt="">
-<div class="flex flex-col w-full">
-    
-            <p class="mt-2 text-2xl h-full flex items-end">{{nomprojet}}</p>
-    <p class="w-1/3">{{ descriptionprojet }}</p>
-</div>
-        </div>
+  <div
+    class="w-full flex justify-center items-center py-10"
+   
+  >
+    <div
+      class="flex w-full h-fit mr-4 flex-row px-[10vw]"
+      :class="{ 'flex-row-reverse': props.isAlternate }"
+    >
+      <img
+        class="object-cover aspect-[16/9] w-1/2 rounded-xl overflow-hidden h-auto"
+        :src="props.imageprojet"
+        alt=""
+      />
+      <div
+        class="flex flex-col w-1/2 pl-10"
+        :class="{ 'pl-0 pr-10': props.isAlternate }"
+      >
+        <p class="h-full flex items-end mb-3 text-balance">
+          {{ props.descriptionprojet }}
+        </p>
+        <p class="w-full text-2xl mb-8">{{ props.nomprojet }}</p>
+        <hr />
+      </div>
+    </div>
+  </div>
 </template>
+
--- src/pages/projets.vue
+++ src/pages/projets.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import { supabase } from '../supabase';
 import CardProjets from '../components/CardProjets.vue';
+
 const { data: Projet, error } = await supabase.from('Projet').select('*');
 console.log(Projet);
 </script>
 
 <template>
-<div v-for="projets in Projet" class="bg-white">
-    
-    <CardProjets v-bind="projets"></CardProjets>
-</div>
+  <div v-for="(projet, index) in Projet" :key="projet.id" class="bg-white">
+    <CardProjets
+      id="Card"
+      v-bind="projet"
+      :isAlternate="index % 2 !== 0"
+    ></CardProjets>
+  </div>
 </template>
