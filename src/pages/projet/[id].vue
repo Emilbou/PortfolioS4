852585f5a42f8d97ee698565770b9785003bf84b
emilbou <emilien.boucard@edu.univ-fcomte.fr>
2024-03-25 17:59:11 +0100

    responsive mibile id

--- src/pages/projet/[id].vue
+++ src/pages/projet/[id].vue
@@ -1,38 +1,3 @@
-<!-- <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
-import { supabase } from '@/supabase'
-import DynamicData from '@/components/DynamicData.vue'
-import FooterPortfolio from '../../components/FooterPortfolio.vue'
-
-const route = useRoute()
-const projet = ref({})
-
-onMounted(async () => {
-  const { data, error } = await supabase
-    .from('Projet')
-    .select('*')
-    .eq('id', route.params.id)
-    .single()
-
-  if (error) {
-    console.error('Erreur lors de la récupération du projet:', error)
-  } else {
-    projet.value = data
-  }
-})
-</script>
-
-<template>
-  <div class="bg-white pt-10 flex justify-center">
-    <RouterLink class="bg-white border p-5 rounded-full" to="/"
-      ><img class="w-20" src="/public/favicon.ico" alt=""
-    /></RouterLink>
-  </div>
-  <DynamicData :id_Projetprops="route.params.id" />
-
-  <FooterPortfolio></FooterPortfolio>
-</template> -->
 <script setup lang="ts">
 import { supabase } from '../../supabase'
 import { useRoute } from 'vue-router'
